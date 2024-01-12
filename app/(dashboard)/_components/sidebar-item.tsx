24bb3e2a1d0e4bed741161b8236718048475a9e3
Dmitriy Belkin <belkin@hrct.ru>
2024-01-12 19:15:57 +0300

    Auth added and build sidebar with interface

---
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { LucideIcon } from "lucide-react";
+
+interface SidebarItemProps {
+    icon: LucideIcon;
+    label: string;
+    href: string;
+}
+
+export const SidebarItem = ({
+    icon,
+    label,
+    href,
+}: SidebarItemProps) => {
+    return ( 
+        <div>
+            Sidebar Item!
+        </div>
+     );
+}
+ 
+export default SidebarItem;
