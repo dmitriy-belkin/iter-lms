a0e51655f63dbbc83a8d9582eaa8dd749e772f5d
Dmitriy Belkin <belkin@hrct.ru>
2024-01-18 15:30:44 +0300

    navbar + search input

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+import { CheckCircle, Clock } from "lucide-react";
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+
+  return (
+    <div className="p-6 space-y-4">
+      <p>Root Page!</p>       
+    </div>
+  )
+}
