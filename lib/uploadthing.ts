7cf1dd7e3d8d35e6c018ffd2c99fa26b85a0d962
Dmitriy Belkin <demetrius.belkin@gmail.com>
2024-02-02 11:19:56 +0300

    categories

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,6 +1,7 @@
-import { generateComponents } from "@uploadthing/react";
- 
+import { generateUploadDropzone, generateUploadButton, generateUploader } from "@uploadthing/react";
+
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
- 
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const Uploader = generateUploader<OurFileRouter>();
